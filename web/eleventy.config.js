87390b2a9754298893c3d49e2b56daeaa7216cc2
Tyler Gaw <me@tylergaw.com>
2026-05-03 12:21:24 -0400

    Roughs out account pages

--- web/eleventy.config.js
+++ web/eleventy.config.js
@@ -20,8 +20,12 @@ export default function (eleventyConfig) {
     hostname: "127.0.0.1",
     middleware: [
       function (req, _res, next) {
+        // Rewrite /account/imports/<job_id> to /account/imports/job/index.html
+        if (req.url.match(/^\/account\/imports\/[^/]+\/?$/)) {
+          req.url = "/account/imports/job/index.html";
+        }
         // Rewrite /profile/<did>/activity/<rkey> to /profile/activity/index.html
-        if (req.url.match(/^\/profile\/[^/]+\/activity\/[^/]+\/?$/)) {
+        else if (req.url.match(/^\/profile\/[^/]+\/activity\/[^/]+\/?$/)) {
           req.url = "/profile/activity/index.html";
         }
         // Rewrite /profile/<handle> to /profile/index.html
