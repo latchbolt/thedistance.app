660985e7f937b0f1999ae41139daef263bf4a0a2
Tyler Gaw <me@tylergaw.com>
2026-05-04 16:58:36 -0400

    Adds capitalize util func

--- web/src/app.js
+++ web/src/app.js
@@ -205,6 +205,10 @@ function formatDate(iso) {
   });
 }
 
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
